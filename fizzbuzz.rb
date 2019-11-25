3d42f58fbaed2f9f142c78d5499edc3cdd944f7a
Steven Frasica <sfrasica1@gmail.com>
2019-11-25 18:23:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif (int % 5 == 0 && int % 3 !== 0)
     "Buzz"
   elsif (int % 3 == 0 && int % 5 == 0)
     "FizzBuzz"
@@ -11,6 +11,3 @@ def fizzbuzz(int)
     puts nil
 end
 end
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
